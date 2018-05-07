04101d5dd54d904a459d2651080a6946b9ecfb7f
jrboyd <jrboyd@med.uvm.edu>
2018-05-07 16:01:36 -0400

    boxes in intersect

--- app.R
+++ app.R
@@ -8,7 +8,95 @@
 #
 
 library(shiny)
+library(shinyjs)
 library(shinydashboard)
+library(magrittr)
+library(shinyFiles)
+library(shinycssloaders)
+library(seqsetvis)
+library(cowplot)
+
+appCSS <- "
+#loading-content {
+  position: absolute;
+  background: #000000;
+  opacity: 0.9;
+  z-index: 100;
+  left: 0;
+  right: 0;
+  height: 100%;
+  text-align: center;
+  color: #FFFFFF;
+}
+"
+
+bw_cache_path = "~/ShinyApps/shiny_peak_data/cached_profiles"
+bed_path = "~/ShinyApps/shiny_peak_data/beds/"
+names(bed_path) = "intersectR"
+
+user_roots = dir("/slipstream/home/", full.names = T) %>% paste0(. , "/ShinyData")
+user_roots = subset(user_roots, dir.exists(user_roots))
+names(user_roots) = dirname(user_roots) %>% basename()
+qcframework_load <<- dir("/slipstream/galaxy/uploads/working/qc_framework", pattern = "^output", full.names = T)
+
+names(qcframework_load) <- basename(qcframework_load)
+roots_load_set = c(bed_path, user_roots, qcframework_load)
+roots_load_set = roots_load_set[dir.exists(roots_load_set)]
+# names(roots_load_set) <- basename(roots_load_set)
+roots_load_bw <<- c("/slipstream/galaxy/production/galaxy-dist/static/UCSCtracks/", 
+                    qcframework_load)
+names(roots_load_bw) <- basename(roots_load_bw)
+roots_load_bw = c(user_roots, roots_load_bw)
+roots_load_bw = roots_load_bw[dir.exists(roots_load_bw)]
+
+roots_save_set = c(bed_path, user_roots)
+roots_save_set = roots_save_set[dir.exists(roots_save_set)]
+
+shinyFiles2load = function(shinyF, roots){
+    root_path = roots[shinyF$root]
+    rel_path = paste0(unlist(shinyF$files), collapse = "/")
+    file_path = paste0(root_path, "/", rel_path)
+    return(file_path)
+}
+
+shinyFiles2save = function(shinyF, roots){
+    root_path = roots[shinyF$root]
+    rel_path = paste0(unlist(shinyF$name), collapse = "/")
+    file_path = paste0(root_path, "/", rel_path)
+    return(file_path)
+}
+
+bw_cache_path = "~/ShinyApps/shiny_peak_data/cached_profiles"
+bed_path = "~/ShinyApps/shiny_peak_data/beds/"
+names(bed_path) = "intersectR"
+
+
+shiny_ssvPlotBox = function(box_title = "Main Plot", id = 1, plot_id = "plotTest", collapsed = TRUE){
+    id = as.character(1)
+    box(title = box_title, collapsible = TRUE, collapsed = collapsed,
+        fluidRow(
+            column(width = 5,
+                   withSpinner(plotOutput(plot_id, width = "280px", height = "280px"))),
+            column(width = 6,
+                   style = "overflow-y:scroll; max-height: 280px",
+                   h1("Settings:"),
+                   sliderInput(paste0("sliderTest-", id) , "Slip'n slide", min = 0, max = 3, value = 1),
+                   textInput(paste0("textTest-", id), "Textin", placeholder = "text here"),
+                   radioButtons(paste0("radioTest-", id), "ABC", LETTERS[1:3]),
+                   h1("asdfasd"),
+                   h1("asdfasd"),
+                   h1("asdfasd"),
+                   h1("asdfasd"),
+                   h1("asdfasd"),
+                   h1("asdfasd"),
+                   h1("asdfasd"),
+                   h1("asdfasd"),
+                   h1("asdfasd"),
+                   p("asdfasd")
+            )
+        )
+    )
+}
 
 header = dashboardHeader(
     
@@ -26,31 +114,33 @@ header = dashboardHeader(
     title = "seqsetvis"
 )
 body <- dashboardBody(
-    fluidRow(
-        tabBox(
-            title = "First tabBox",
-            # The id lets us use input$tabset1 on the server to find the current tab
-            id = "tabset1", height = "250px",
-            tabPanel("Tab1", "First tab content"),
-            tabPanel("Tab2", "Tab content 2")
+    tabItems(
+        tabItem(tabName = "load",
+                h2("Load"),
+                tags$hr(),
+                h3("Pick a bed file (ideally after annotating with peak_annotatR)"),
+                tags$hr(),
+                shinyFilesButton(id = "FilesLoadSet", label = "Find Files on Server", title = "Find Peaks to Annotate", multiple = F),
+                fileInput(inputId = "UploadLoadSet", label = "Browse Local Files"),
+                tags$hr()
         ),
-        tabBox(
-            side = "right", height = "250px",
-            selected = "Tab3",
-            tabPanel("Tab1", "Tab content 1"),
-            tabPanel("Tab2", "Tab content 2"),
-            tabPanel("Tab3", "Note that when side=right, the tab order is reversed.")
-        )
-    ),
-    fluidRow(
-        tabBox(
-            # Title can include an icon
-            title = tagList(shiny::icon("gear"), "tabBox status"),
-            tabPanel("Tab1",
-                     "Currently selected tab from first box:",
-                     verbatimTextOutput("tabset1Selected")
-            ),
-            tabPanel("Tab2", "Tab content 2")
+        tabItem(tabName = "intersect",
+                h2("Intersect"),
+                actionButton("redrawPlot", label = "Redraw"),
+                tags$br(),
+                tags$br(),
+                fluidRow(
+                    # tags$div(
+                    shiny_ssvPlotBox("Bars", 1, "plotTest1", collapsed = FALSE),
+                    shiny_ssvPlotBox("Euler", 2, "plotTest2"),
+                    shiny_ssvPlotBox("Membership Map", 3, "plotTest3"),
+                    shiny_ssvPlotBox("Venn", 4, "plotTest4"),
+                    shiny_ssvPlotBox("Line - aggregated", 5, "plotTest5")
+                    
+                )
+        ),
+        tabItem(tabName = "inspect",
+                h2("Inspect")       
         )
     )
 )
@@ -60,22 +150,84 @@ sidebar = dashboardSidebar(
 )
 
 shinyApp(
-    ui = dashboardPage(
-        header,
-        sidebar,
-        body
-    ),
-    server = function(input, output) {
+    ui = fluidPage(
+        useShinyjs(),
+        inlineCSS(appCSS),
+        div(
+            id = "loading-content",
+            h2("Loading...")
+        ),
+        hidden(
+            div(
+                id = "app-content",
+                dashboardPage(
+                    header,
+                    sidebar,
+                    body
+                )))),
+    server = function(input, output, session) {
+        # Stop app when browser tab closed
+        session$onSessionEnded(stopApp)
+        # Hide the loading message when the rest of the server function has executed
+        hide(id = "loading-content", anim = TRUE, animType = "fade")    
+        show("app-content")
+        
         # The currently selected tab from the first box
+        shinyFileChoose(input, 'FilesLoadSet', 
+                        roots= roots_load_set, 
+                        filetypes=c("bed", "txt", "Peak"))
+        
+        shinyFileChoose(input, 'FilesLoadBigwig', 
+                        roots= roots_load_bw, 
+                        filetypes=c("bigwig", "bw"))
+        
+        shinyFileSave(input, 'FilesSaveSet', 
+                      roots= roots_save_set, 
+                      filetypes=c("bed"))
+        
+        pcols = safeBrew(3, pal = "Dark2")
+        
+        output$plotTest1 = renderPlot(width = 280, height = 280, {
+            input$redrawPlot
+            ssvFeatureBars(CTCF_in_10a_overlaps_gr, bar_colors = pcols) + 
+                guides(color = "none")
+        })
+        
+        output$plotTest2 = renderPlot(width = 280, height = 280, {
+            input$redrawPlot
+            ssvFeatureEuler(CTCF_in_10a_overlaps_gr, circle_colors = pcols) + 
+                guides(color = "none", fill = "none")
+        })
+        
+        output$plotTest3 = renderPlot(width = 280, height = 280, {
+            input$redrawPlot
+            ssvFeatureBinaryHeatmap(CTCF_in_10a_overlaps_gr) + 
+                guides(color = "none")
+        })
+        
+        output$plotTest4 = renderPlot(width = 280, height = 280, {
+            input$redrawPlot
+            ssvFeatureVenn(CTCF_in_10a_overlaps_gr) + 
+                scale_color_manual(values = safeBrew(3, pal = "Dark2")) +
+                guides(color = "none", fill = "none")
+        })
+        
+        output$plotTest5 = renderPlot(width = 280, height = 280, {
+            input$redrawPlot
+            ssvSignalLineplotAgg(CTCF_in_10a_profiles_dt, spline_n = 5) + 
+                scale_color_manual(values = safeBrew(3, pal = "Dark2")) +
+                theme_cowplot() + guides(color = "none")
+        })
+        
         output$tabset1Selected <- renderText({
             input$tabset1
         })
         
         output$menu <- renderMenu({
             sidebarMenu(
-                menuItem("load", icon = icon("file-o", lib = "font-awesome"), badgeLabel = "!", badgeColor = "red"),
-                menuItem("intersect", icon = icon("bar-chart", lib = "font-awesome"), badgeLabel = "!", badgeColor = "red"),
-                menuItem("inspect", icon = icon("area-chart", lib = "font-awesome"), badgeLabel = "!", badgeColor = "red")
+                menuItem("Load", tabName = "load", icon = icon("file-o", lib = "font-awesome"), badgeLabel = "!", badgeColor = "red"),
+                menuItem("Intersect", tabName = "intersect", icon = icon("bar-chart", lib = "font-awesome"), badgeLabel = "!", badgeColor = "red"),
+                menuItem("Inspect", tabName = "inspect", icon = icon("area-chart", lib = "font-awesome"), badgeLabel = "!", badgeColor = "red")
             )
         })
     }
